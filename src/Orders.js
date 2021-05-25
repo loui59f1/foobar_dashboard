9e868e5e5df7539e225d061f19dc1bb13d42073b
Annemette <anne7n62@stud.kea.dk>
2021-05-25 11:40:45 +0200

    test

--- src/Orders.js
+++ src/Orders.js
@@ -1,25 +1,23 @@
 export default function Orders({ queue }) {
+  const orderList = queue.map((order) => <Order order={order} />);
 
-    const orderList = queue.map((order) => <Order order={order} />);
-
-    return (
-        <div className="Orders">
-            <h1>Incoming orders</h1>
-            <div className="OrderList">{orderList}</div>
-        </div>
-    );
+  return (
+    <div className="Orders">
+      <h1>Incoming orders</h1>
+      <div className="OrderList">{orderList}</div>
+    </div>
+  );
 }
 
 function Order({ order }) {
+  // Hvordan får vi håndteret flere af samme øl?
+  // Ex. 2 x Mowintime, 1 x El Hefe don no
 
-    // Hvordan får vi håndteret flere af samme øl?
-    // Ex. 2 x Mowintime, 1 x El Hefe
-
-    return (
-        <div className="Order">
-            <h3>Order id: {order.id}</h3>
-            <p>{order.order}</p>
-            <p>Table number</p>
-        </div>
-    );
+  return (
+    <div className="Order">
+      <h3>Order id: {order.id}</h3>
+      <p>{order.order}</p>
+      <p>Table number</p>
+    </div>
+  );
 }
