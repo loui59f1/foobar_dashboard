7da5f9da3ea77698a4a5469e0b700e007c35eae7
Louise <loui59f1@stud.kea.dk>
2021-05-31 16:04:20 +0200

    Further styling

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 export default function Header({ bar }) {
     return (
         <header>
-            <img src={`./img/foobar_logo.svg`} className="logo" alt="Logo" />
+            <img src={`img/foobar_logo.svg`} className="logo" alt="Logo" />
             <div className="info">Closing at {bar.closingTime}</div>
             <div className="dashboard_navigation">
                 <button><span className="notification_btn"></span></button>
